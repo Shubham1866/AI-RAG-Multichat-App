a795e03d44179936dbb9c24c4b4ff9035ed0af27
Shubham Bairagi <84385664+Shubham1866@users.noreply.github.com>
2026-05-06 14:28:30 +0530

    Change DATABASE_URL to a placeholder
    Updated DATABASE_URL to a placeholder for security.

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Shubham1866@localhost:3306/rag_chat_db"
+DATABASE_URL = "YOUR DATABASE CONNECTION URL"
 
 #Connection
 engine = create_engine(
